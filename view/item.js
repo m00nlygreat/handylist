5e7151ba061c9e898cc81d2f47f173724bfcdc7c
m00nlygreat <moonlygreat@gmail.com>
2024-08-22 17:01:05 +0900

    넘침 수정

--- view/item.js
+++ view/item.js
@@ -3,7 +3,7 @@ const md = require('marked');
 
 module.exports = item => {
     return `<li class="pl-4 pr-2 py-2 b-gray-200 b-1 b-solid rounded-md mb-2 items-center flex space-x-4 w-full overflow-clip">
-        <div class="md w-full overflow-clip">
+        <div class="md w-full overflow-hidden min-w-0">
         ${md.parse(item.content)}
         </div>
         <a class="decoration-none" href="./delete_item/${item.board_id}/${item.id}"><div class="w-8 text-gray-300 aspect-square flex justify-center items-center text-center text-lg">×</div></a>
